fcd4fed9ebef99f7898c7b098e01752e763749d7
Reechka <R2echka@gmail.com>
2024-06-24 23:35:25 +0300

    Топ-5 транзакций по сумме

--- src/views.py
+++ src/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+
 import pandas as pd
 
 
@@ -15,22 +16,42 @@ def greeting(dt_: str) -> str:
     else:
         return "Добрый вечер"
 
+
 def card_info(filename: str) -> dict:
+    """Информация по каждой карте в формате словаря"""
     data = pd.read_excel(filename)
-    cards = set(data['Номер карты'])
+    cards = set(data["Номер карты"])
     card_info_list = []
     for card in cards:
-        if str(card)[0] == '*':
+        if str(card)[0] == "*":
             last_digits = str(card)[1:]
             total_spent = 0.0
-            for _, row in data[data['Номер карты'] == card].iterrows():
-                total_spent += row['Сумма платежа']
+            for _, row in data[data["Номер карты"] == card].iterrows():
+                total_spent += row["Сумма платежа"]
             cashback = total_spent % 100
             info = {
                 "last_digits": last_digits,
                 "total_spent": abs(total_spent),
-                "cashback": int(cashback)
+                "cashback": int(cashback),
             }
             card_info_list.append(info)
-    result = {"cards": card_info_list}
-    return result
+    return {"cards": card_info_list}
+
+
+def top_transactions(filename: str) -> list:
+    """Выводит топ-5 транзакций в формате словаря"""
+    data = pd.read_excel(filename)
+    transactions = data.sort_values(
+        "Сумма операции", key=lambda x: abs(x), ascending=False
+    )
+    top = []
+    for _, row in transactions.head().iterrows():
+        top.append(
+            {
+                "date": row["Дата операции"][:10],
+                "amount": abs(row["Сумма операции"]),
+                "category": row["Категория"],
+                "description": row["Описание"],
+            }
+        )
+    return top
