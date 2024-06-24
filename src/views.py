93c2ceb4198d6ecdffcac3a70e490ea76b547548
Reechka <R2echka@gmail.com>
2024-06-24 19:00:00 +0300

    Информация по картам

--- src/views.py
+++ src/views.py
@@ -1,19 +1,36 @@
 from datetime import datetime as dt
+import pandas as pd
+
 
 def greeting(dt_: str) -> str:
-    formated_date = dt.strptime(dt_, '%Y-%m-%d %H:%M:%S')
-    hour = int(formated_date.strftime('%H'))
+    """Приветствие программы в зависимости от времени"""
+    formated_date = dt.strptime(dt_, "%Y-%m-%d %H:%M:%S")
+    hour = int(formated_date.strftime("%H"))
     if hour < 7:
-        print(hour)
         return "Доброй ночи"
     elif 6 < hour < 13:
-        print(hour)
         return "Доброе утро"
     elif 12 < hour < 19:
-        print(hour)
         return "Добрый день"
     else:
-        print(hour)
         return "Добрый вечер"
 
-print(greeting('2024-06-23 23:08:50'))
+def card_info(filename: str) -> dict:
+    data = pd.read_excel(filename)
+    cards = set(data['Номер карты'])
+    card_info_list = []
+    for card in cards:
+        if str(card)[0] == '*':
+            last_digits = str(card)[1:]
+            total_spent = 0.0
+            for _, row in data[data['Номер карты'] == card].iterrows():
+                total_spent += row['Сумма платежа']
+            cashback = total_spent % 100
+            info = {
+                "last_digits": last_digits,
+                "total_spent": abs(total_spent),
+                "cashback": int(cashback)
+            }
+            card_info_list.append(info)
+    result = {"cards": card_info_list}
+    return result
